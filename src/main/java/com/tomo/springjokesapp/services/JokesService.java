148e68a67e0ae3c88b84f7469e908acd4ce4952b
Tomo <tomo.vucic@gmail.com>
2022-01-10 21:39:04 +0100

    Joke Service layer created. Closes #3 issue.

---
+++ src/main/java/com/tomo/springjokesapp/services/JokesService.java
@@ -0,0 +1,6 @@
+package com.tomo.springjokesapp.services;
+
+public interface JokesService
+{
+    String getJoke();
+}
---
+++ src/main/java/com/tomo/springjokesapp/services/JokesServiceImpl.java
@@ -0,0 +1,21 @@
+package com.tomo.springjokesapp.services;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JokesServiceImpl implements JokesService
+{
+    private final ChuckNorrisQuotes chuckNorrisQuotes;
+
+    public JokesServiceImpl()
+    {
+        this.chuckNorrisQuotes = new ChuckNorrisQuotes();
+    }
+
+    @Override
+    public String getJoke()
+    {
+        return chuckNorrisQuotes.getRandomQuote();
+    }
+}
